71d6a3d7b5ead72426352219b5f4a5d6eee959f2
mathew <mathew_holland@hotmail.co.uk>
2025-02-05 14:20:23 +0000

    added the main while loop to keep the app running, and some match case statements

---
+++ main.py
@@ -0,0 +1,16 @@
+todos = []
+
+while True:
+    user_action = input("Type add, show or exit: ").strip().lower()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ").strip()
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                print(item)
+        case 'exit':
+            break
+
+print("Bye!")
